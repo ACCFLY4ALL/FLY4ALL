d2e63a3856ebcc8e53ad9645ce8609f04c07533a
acc.alrawdaten <acc.alrawdaten@gmail.com>
2025-10-06 05:32:09 +0000

    ١

---
+++ src/lib/firebase-admin-config.ts
@@ -0,0 +1,13 @@
+export const firebaseAdminConfig = {
+  "type": "service_account",
+  "project_id": "acc-mangmants",
+  "private_key_id": "aa073057acd7fa54ad6d01f8f4dfb5967fbdcbab",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDaLNCFHq2X8JLh\nPAUZv2xttpkjS1kSVs/tMkBOP42VrcQ1FXf7ALwnSHA7xvPrEWmBzS+bvQ85R7zW\nLcC1WUnNYI98OoyhwkivhF2eibBuDwah79TWTZ/1nzhiLJTcfRFdf2dmta3sAkCn\nbJ4Fff4qgeaRHycl/tI+f6Z+VTk4WTBjtZ+wyXDciCMUDZWKE7vr/dVoN2JYuBup\n96NAV8ASx4AorpfYGnIf7BPXXikhcf0L8e8C2vFmreAgJ/f6hP0n1l9xrsLpZJKT\nOsdfDwMvALLho3g0QGr8pXL6ScMvsHTE4wCrq5j1Wz0fM9LefgVrct0hPzGIU7Xq\nw8fAF0fpAgMBAAECgf8QwrXs2W58kv61pDwVaPSWI8ZKmn89PsNlICHQi7v/bln5\nugHS8TuDHmLWNMfdnHjT1UTTB13Os17OarVjOSNyTk4M58fl+rGayjsRnYJFnOHl\njtx1miuvsuD3VHVwetXarkRDSlxnb8TdF9BhRkyVog+/FlBA8FKLz+1PxzxIwmz2\nApR0dI7k25CNcz1OMfYpnbdPsqgqwcACRr4rlrUF+gdnlemnrfw4l9mlj20E4h1g\niFymr8VcChjMvnZizBk+w6lGO5nfW3+vp0WpHBgkP7KgNZjLLQCOoJPAy8G/iT6G\n5085OQZBJhwg23qqE8n175sROrdwyaM8Nk1fWQECgYEA9BN7+xfMRrapcaVSIJby\nUQ6XQaomkCwn3xMAk9D5xfHQ/yymG2gj+ghbhdSHxQ+8rmHlVYxdmNTmFAmEjLm7\n0KTSNUmXpa09MaOtIYToBiNE0wTmWJRyRNeubgKM6tu6KwdJWZv44cw3mg7+TNR/\nqftGiQXrnn2igtNDLNIQr8kCgYEA5NVmq4/aSFAvgLP2kio3+bvY+HvuhdS8tMqw\nh3O6sRO5E+Ktk1bldQuyVtIa3rKQ2nY7YwC0mQ29hGsmg5iGHvFGd8FVe1JjoXQU\nsx7xxbTTqRZaxHdf/jslAAQ8tzYdb/hsDYrwkpNbo8s2Sy4j2MYx+PIVTZNQuU2G\noFZhJyECgYBjLjO4MdBxf7JVGu/HjG8FHx+5sDGhbj/5dthKOKX2mK/rsTICewv7\ngz/lnxpYqm9qkR9G5xX1j7W6uK9jG+pE+2TsAG6eBUqTjEZDE6XRMK277gbIQfI5\nJUedLm4i1YzOBVT6E9ZLstd5Q9R0VflWkmP0NDKtobHS5ZQrd2UOeQKBgQC0vime\nEL1kqJEcowh6YoZb5pd1jM3VPzbdxcqw8iPfgaiJoF1hJx2N2XM/SCyZAYmQRBqc\nFA6RZVzvnoKJJoupHQF/zWDrSyQOflVrNWHZuv4Da/i9fVyAvc53uOEMBN4zdpBC\nEh/6ZhHXycZUk1kU+1a1PuC8qUL1WdqgAhoygQKBgEY0k+jkRChVune9v375x+DZ\nMnK/B90FIItTr/X4cljAQNuBtZ1WFg5RL34N60Sf6Up2O8ZPpZP1rEThDWbSEZ3o\n+5BMTg76hO0C+AfV6aoCQbWW/XeHEcb8sEu6NbT98qIFJWOGfnL+PhJYgRCMj8uW\nSyfiPvtf5f0+UjfefO6Q\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@acc-mangmants.iam.gserviceaccount.com",
+  "client_id": "109673701002630124774",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40acc-mangmants.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
